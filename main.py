30aaa800e50ce14dfa26fe5d29218dc58c43ddc5
taurinese <enzodylan.taurines@gmail.com>
2020-06-22 12:17:27 +0200

    Added background and resized window

--- main.py
+++ main.py
@@ -5,12 +5,20 @@ pygame.init()
 # Générer la fenêtre de jeu
 ## Titre , icône (à rajouter plus tard si possible)
 pygame.display.set_caption('Pokémon Fight')
-pygame.display.set_mode((1080, 720))
+screen = pygame.display.set_mode((1050, 540))
 
 running = True
 
+#background du jeu
+background = pygame.image.load('assets/Images/fight_bg.jpg')
 
 while running:
+
+    # Set le background
+    screen.blit(background, (0, 0))
+
+    # Mettre à jour l'écran
+    pygame.display.flip()
 
     # Si la fenêtre est fermée
     for event in pygame.event.get():
